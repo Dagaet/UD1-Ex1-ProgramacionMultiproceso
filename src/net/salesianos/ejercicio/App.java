abc3bb1be56ccc26111c2dec6d268ef9915a09c2
SaulArteaga <alojera15@gmail.com>
2023-10-11 11:23:30 +0100

    feat(App): added processLauncher & informations

--- src/net/salesianos/ejercicio/App.java
+++ src/net/salesianos/ejercicio/App.java
@@ -1,7 +1,30 @@
 package net.salesianos.ejercicio;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import net.salesianos.ejercicio.launcher.ProcessLauncher;
+import net.salesianos.ejercicio.utils.Utils;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        File outputFile = ProcessLauncher.OUTPUT_FILE;
+        String[] VOWELS = {"a", "e", "i", "o", "u"};
+        ArrayList<Process> allProcesses = new ArrayList<>();
+
+        for (String vowel : VOWELS) {
+            Process subprocesses = ProcessLauncher.initVowelProcess(vowel);
+            allProcesses.add(subprocesses);
+        }
+        for (Process process : allProcesses) {
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Utils.textObtainer();
+        Utils.vowelPrinter(outputFile, VOWELS);
     }
 }
